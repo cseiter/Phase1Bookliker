70523605f4200147f04ead14a0a3d98918ee904b
chris <cseiter@gmail.com>
2021-04-16 20:45:50 -0400

    first commit, plan out javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {});
+//1. Get a list of books and render them from http://localhost:3000/books
+//fetch function returns promise
+//append parent UL with LI element of each book title returned
+//2. Be able to click on a book and see the thumbnail, description, and a list of users who liked
+//use event handlers to turn LI into clickable item (hover color to show actionable?)
+//show thumbnail, title, subtitle, author, description, and UL of users who liked
+//3. Like a book by clicking on a button.
+//add button element (next to book so it doesn't move?)
+//use PATCH and append your user, "{"id":1, "username":"pourus"}, to array of existing likes
+//BONUS. unlike a book by clicking same button
+//search current like array for your user, if found, change button to unlike
+//PATCH request removes your user and updates
